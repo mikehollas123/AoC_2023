25bc6b66b6559b99e745749c6a99662eb68ee013
Mikehollas123 <hollas@gmail.com>
2023-12-11 11:39:53 -0600

    started day 9 - +3 off for part 1. not sure why...

--- day-08/src/main.rs
+++ day-08/src/main.rs
@@ -2,7 +2,7 @@ mod part_1;
 mod part_2;
 
 fn main() {
-    let result1 = part_1::process(include_str!("input.txt"));
+    let result1 = part_1::process(include_str!("test.txt"));
     print!("day-01 part1: {}\n",result1);
 
     // I had to look this up, the brute force method was never going to work!
---
+++ day-09/src/part_1.rs
@@ -0,0 +1,48 @@
+
+pub fn process(input : &str) -> String {
+
+    let mut total_predicted = 0;
+    let mut pre = vec![];
+
+    for line in input.lines(){
+
+        let data: Vec<_> = line.trim().split(" ").map(|x| x.parse::<i64>().unwrap()).collect();
+        //println!("{:?}",data);
+        let mut last_diff = vec![];
+        let mut diff =get_diffs(&data);
+        //println!("{:?}",diff);
+        last_diff.push(diff[diff.len() -1]);
+
+        while diff[diff.len() -1] != 0{
+            diff = get_diffs(&diff);
+            last_diff.push(diff[diff.len() -1]);
+            //println!("{:?}",diff);
+        }
+        for i in (1..last_diff.len()).rev(){
+            last_diff[i-1] = last_diff[i] + last_diff[i-1];
+        }
+
+        let predicted = last_diff[0] + data[data.len() -1];
+
+        //println!("{:?}",last_diff);
+        println!("{:?}", predicted);
+
+        total_predicted += predicted;
+
+        pre.push(predicted);
+    }
+    //println!("{}",pre.iter().sum::<i64>());
+
+    return format!("{}",total_predicted);
+}
+
+fn get_diffs(data: &Vec<i64>)-> Vec<i64>{
+
+    let mut diffs = vec![];
+    for i in 0..data.len()-1{
+        diffs.push(data[i+1] - data[i]);
+    }
+
+    diffs
+}
+
